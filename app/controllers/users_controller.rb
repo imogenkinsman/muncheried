390b76f8d698a1e808d904742523acdeb333f9d8
Jade McGough <jade@thezets.com>
2014-02-04 20:45:51 -0800

    added users/new, added users routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Success!'
+    end
+    redirect_to root
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Muncheried::Application.routes.draw do
   get '/faq',   to: 'static#faq'
 
   # user routes
-  get '/manage', to: 'users#manage'
+  resources :users, only: [:new, :create, :destroy]
 
 end
