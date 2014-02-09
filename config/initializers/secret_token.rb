b41f63611c4665062ac595d1029ad8a17294c7e4
Jade McGough <jade@thezets.com>
2014-02-09 04:02:02 -0800

    changing and removing secret token from source control

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Muncheried::Application.config.secret_key_base = 'ae622fc90588686657a37c51881870ce28c848fc68ad3c11416f07ec1d296f75f44e360ca2de09abff96b556465ba8c2c1c559b886d963d76b41a05e9fe0e474'
+Muncheried::Application.config.secret_key_base = ENV['SECRET_TOKEN']
