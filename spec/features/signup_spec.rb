6258501dcebce9a966f15e81248c52247675e3b4
Jade McGough <jade@thezets.com>
2014-02-17 07:22:41 -0800

    finish capybara test

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,8 +3,8 @@ feature "Signing Up" do
   scenario "Signing up with valid email" do
     visit root_path
     fill_in 'email', with: 'test@thezets.com'
-    click_button 'Submit'
-    #expect
+    expect{click_button 'Submit'}.to change(User, :count).from(0).to(1)
+    expect(page).to have_content 'Successfully added test@thezets.com'
   end
 
 end
