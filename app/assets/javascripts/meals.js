a2f85cff955d065e74239a7b7e8e7b215859def8
Jade McGough <jade@thezets.com>
2014-02-17 07:00:00 -0800

    ajax response working :D

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -3,7 +3,9 @@ $(function() {
 
 $('#email-form').on('ajax:success', function(data, status, xhr) {
   $('#flash-messages').remove();
-  $('nav').after("<p>Success</p>");
+  // TODO: pass a partial from the json response or something, this is hacky
+  $('nav').after("<div id='flash-messages'><ul><li class='" + status.type + "'>" + status.message + "</li></ul></div>");
+  $('#flash-messages').slideDown();
 });
 
 $('#email-form').on('ajax:error', function(event, xhr, status) {
