2f9197759b317ab430a50f238c0693122a5c43ea
Jade McGough <jade@thezets.com>
2014-02-20 18:24:43 -0800

    improve tests and coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,20 +2,26 @@ describe UsersController do
 
   describe 'post #create' do
 
+    let(:email) { 'test@userscontroller.com'}
+
     it 'creates a new user' do
-      expect{ post :create, email: 'a@userscontroller.com' }.to change{ User.count }.from(0).to(1)
+      expect{ post :create, email: email }.to change{ User.count }.from(0).to(1)
     end
 
-    it 'gives a json response' do
-      email = 'c@userscontroller.com'
+    it 'gives a json response for successes' do
       post :create, email: email
       expect(response).to be_success
       expect(response.body).to eq({ type: 'success', message: "Successfully added #{email}"}.to_json)
     end
 
+    it 'gives a json response for failures' do
+      2.times { post :create, email: email }
+      expect(response.body).to eq({ type: 'error', message: "Unable to add #{email}"}.to_json)
+    end
+
     it 'creates a secret key for the user' do
-      post :create, email: 'c@userscontroller.com'
-      user = User.find_by(email: 'c@userscontroller.com')
+      post :create, email: email
+      user = User.find_by(email: email)
       expect(user.secret_key.length).to eq 22
     end
 
@@ -23,28 +29,26 @@ describe UsersController do
 
   describe 'get #unsubscribe' do
 
-    before :each do
-      @user = create(:user)
-    end
+    let(:user) { create(:user) }
 
     it 'unsubscribes a user' do
-      expect(@user.subscribed).to be_true
-      get :unsubscribe, { id: @user.id, key: @user.secret_key }
+      expect(user.subscribed).to be_true
+      get :unsubscribe, { id: user.id, key: user.secret_key }
 
-      @user.reload
+      user.reload
 
-      expect(@user.subscribed).to be_false
+      expect(user.subscribed).to be_false
       expect(response).to redirect_to root_path
-      expect(flash[:success]).to eq "Successfully unsubscribed #{@user.email} from MunchAlerts"
+      expect(flash[:success]).to eq "Successfully unsubscribed #{user.email} from MunchAlerts"
     end
 
     it "does not unsubscribe a user if secret key doesn't match" do
-      expect(@user.subscribed).to be_true
-      get :unsubscribe, { id: @user.id, key: '12345' }
+      expect(user.subscribed).to be_true
+      get :unsubscribe, { id: user.id, key: '12345' }
 
-      @user.reload
+      user.reload
 
-      expect(@user.subscribed).to be_true
+      expect(user.subscribed).to be_true
     end
 
   end
